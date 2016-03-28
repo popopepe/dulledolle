bfe8321457c0b0af13873c9210da7fa95d49e86a
popopepe <88quintilis@gmail.com>
2016-03-28 08:17:10 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dulledolle::Application.config.secret_key_base = '9aa3b41b84906022015be1a795562b7565e14d8e3531684d37c9b55c8095cd62fdebba89152145ccb4d5cc44e19b5be1e263624f5066edfd77bd39467cd27b07'
